b2d62c4916eea62f0e0a13acf6b7334184efcf2e
BetoCR16 <48104122+BetoCR16@users.noreply.github.com>
2025-11-13 22:46:23 -0600

    add: assign conservation areas to fire detections

--- app.py
+++ app.py
@@ -23,8 +23,36 @@ def cargar_datos_incendios():
 
 datos_incendios = cargar_datos_incendios()
 
+# Carga de Áreas de conservación de Costa Rica
+# Conexión al servicio WFS
+wfs_url = 'http://geos1pne.sirefor.go.cr/wfs'
+wfs_version = '1.1.0'
+wfs = WebFeatureService(url=wfs_url, version=wfs_version)
+
+# Obtener la capa de áreas de conservación
+capa = 'PNE:areas_conservacion'
+respuesta = wfs.getfeature(typename=capa, outputFormat='application/json')
+
+# Leer la respuesta en un GeoDataFrame y convertir a CRS 4326
+areas_conservacion_gdf = gpd.read_file(BytesIO(respuesta.read())).to_crs(epsg=4326)
+
 
 ### Organizacion de datos
+
+# Convertir datos de incendios a geodataframe
+datos_incendios_gdf = gpd.GeoDataFrame(
+    datos_incendios,
+    geometry=gpd.points_from_xy(datos_incendios['longitude'], datos_incendios['latitude']),
+    crs="EPSG:4326"
+)
+
+# Unir incendios a las areas
+datos_incendios_por_area = gpd.sjoin(
+    datos_incendios_gdf,
+    areas_conservacion_gdf,
+    how="left",
+    predicate="within"
+)
 
 ## Conversion de fechas
 datos_incendios['acq_date'] = pd.to_datetime(datos_incendios['acq_date'])
@@ -56,6 +84,8 @@ datos_incendios = datos_incendios.rename(columns={
     'confidence': 'Confianza',
     'daynight': 'Día/Noche'
 })
+
+
 
 ## TABLA
 st.subheader('Datos de incendios en Costa Rica 2020 - 2024')
