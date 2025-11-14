ab21bc447898e5e805ba4cbedf3c973286cbe81a
BetoCR16 <48104122+BetoCR16@users.noreply.github.com>
2025-11-13 22:35:10 -0600

    improve: organization of data for table

--- app.py
+++ app.py
@@ -23,20 +23,52 @@ def cargar_datos_incendios():
 
 datos_incendios = cargar_datos_incendios()
 
-datos_incendios['acq_date'] = pd.to_datetime(datos_incendios['acq_date'])
 
+### Organizacion de datos
+
+## Conversion de fechas
+datos_incendios['acq_date'] = pd.to_datetime(datos_incendios['acq_date'])
+datos_incendios['acq_time'] = datos_incendios['acq_time'].astype(str).str.zfill(4)
+datos_incendios['complete_date'] = pd.to_datetime(
+    datos_incendios['acq_date'].dt.strftime('%Y-%m-%d') + ' ' +
+    datos_incendios['acq_time'].str[:2] + ':' +
+    datos_incendios['acq_time'].str[2:]
+)
+
+# Columnas relevantes
+columnas = [
+    'complete_date',
+    'latitude', 
+    'longitude', 
+    'brightness',
+    'confidence',
+    'frp',
+    'daynight'
+]
+datos_incendios = datos_incendios[columnas]
+
+datos_incendios = datos_incendios.rename(columns={
+    'complete_date': 'Fecha',
+    'latitude': 'Latitud',
+    'longitude': 'Longitud',
+    'brightness': 'Brillo',
+    'frp': 'FRP',
+    'confidence': 'Confianza',
+    'daynight': 'Día/Noche'
+})
+
+## TABLA
 st.subheader('Datos de incendios en Costa Rica 2020 - 2024')
 st.dataframe(datos_incendios, hide_index=True)
 
-
-### Grafico
+### GRAFICO
 
 # Creación de columna con solo el año
-datos_incendios['year'] = datos_incendios['acq_date'].dt.year
+datos_incendios['Año'] = datos_incendios['Fecha'].dt.year
 
 # Conteo de incendios por cada año
 conteo_incendios_por_anyo = (
-    datos_incendios.groupby('year')
+    datos_incendios.groupby('Año')
     .size()
     .reset_index(name='Frecuencia')
     .rename(columns={'year': 'Año'})
@@ -54,3 +86,4 @@ fig_incendios_anuales.update_xaxes(dtick=1)
 # Mostrar el gráfico
 st.subheader('Tendencia de focos de calor (incendios) en Costa Rica (2020-2024)')
 st.plotly_chart(fig_incendios_anuales)
+
